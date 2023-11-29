acea5f2763e3661403190121ce2fbd2658a9be02
BenJammin <bmonty124@outlook.com>
2023-11-29 17:26:54 -0500

    final

--- script.js
+++ script.js
@@ -38,16 +38,37 @@ let colorSelected;
 const colorSelect = () => {
     colorSelected = color.value;
     return colorSelected
-
 }
 
+let getRandomNum = () => {
+    return Math.floor(Math.random() * 255) + 1;
+}
 
+let getRandomColor = () => {
+    let randy = getRandomNum();
+    let randy2 = getRandomNum();
+    let randy3 = getRandomNum();
+    return `rgb(${randy}, ${randy2}, ${randy3})`
+}
 
 let reset = document.querySelector('#reset')
 reset.addEventListener('click', e => {
     canvas.innerHTML = '';
-    canvas.style.gridTemplateColumns = `repeat(${sizeSelect}, 1fr)`
-    canvas.style.gridTemplateRows = `repeat(${sizeSelect}, 1fr)`
+    if (typeof sizeSelect === 'undefined') {
+        canvas.style.gridTemplateColumns = `repeat(2, 1fr)`
+        canvas.style.gridTemplateRows = `repeat(2, 1fr)`
+        for (let i = 1; i <= 4; i++) {
+            div = document.createElement('div');
+            div.classList.add('square');
+            canvas.append(div);
+        }
+    } else if (!isNaN(sizeSelect)) {
+        canvas.style.gridTemplateColumns = `repeat(${sizeSelect}, 1fr)`
+        canvas.style.gridTemplateRows = `repeat(${sizeSelect}, 1fr)`
+    }
+
+    // canvas.style.gridTemplateColumns = `repeat(${sizeSelect}, 1fr)`
+    // canvas.style.gridTemplateRows = `repeat(${sizeSelect}, 1fr)`
     for (let i = 1; i <= (sizeSelect * sizeSelect); i++) {
         div = document.createElement('div');
         div.classList.add('square');
@@ -57,12 +78,23 @@ reset.addEventListener('click', e => {
 })
 
 
+// let opac = 0;
+// let darker = (sq) => {
+//     opac += .25;
+//     sq.style.backgroundColor = `rgba(0, 0, 0, ${opac})`
+// }
 
 
 console.log(canvas.childElementCount)
 
 canvas.addEventListener('mouseover', e => {
-    e.target.style.backgroundColor = colorSelect();
+    let rand = colorSelect();
+    if (rand == 'red' || rand == 'blue' || rand == 'pink') {
+        e.target.style.backgroundColor = rand;
+    } else if (rand == 'randy') {
+        e.target.style.backgroundColor = getRandomColor();
+    }
+
 })
 
 let count = canvas.childElementCount
